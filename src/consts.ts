6bb6d1825c681f89a5ce593d9c67db8c6b70a916
himanshubhardwaz <himanshu76200@gmail.com>
2025-03-14 10:00:01 +0530

    feat: added base styles from https://bearblog.dev/

---
+++ src/consts.ts
@@ -0,0 +1,33 @@
+export const SITE_TITLE = "blog.himanshubhardwaz.com";
+export const SITE_DESCRIPTION = "Welcome (. ❛ ᴗ ❛.)";
+export const COPYRIGHT = `© ${new Date().getFullYear()} PlainPage All Rights Reserved`;
+
+export const SOCIAL_LINKS = {
+  Github: "https://github.com/himanshubhardwaz",
+  Twitter: "https://x.com/himanshu_bz",
+  LinkedIn: "https://www.linkedin.com/in/himanshubhardwaz",
+  Instagram: "https://www.instagram.com/_himanshu.bhardwaz_",
+};
+
+export const SEO_CONFIG = {
+  ogImage: "/hero-img.png",
+  keywords: "blog, tech, programming",
+};
+
+export const NAV_ITEMS = [
+  { text: "Home", link: "/" },
+  { text: "Blog", link: "/blog" },
+  { text: "Tags", link: "/tags" },
+  { text: "About", link: "/about" },
+];
+
+export const BLOG_CONFIG = {
+  locale: "en-us",
+  profile: "https://github.com/himanshubhardwaz",
+  authorName: "Author Name",
+  email: "mailto:your-email@example.com",
+  tags: {
+    title: "Tags",
+    description: "All the tags used in posts.",
+  },
+};

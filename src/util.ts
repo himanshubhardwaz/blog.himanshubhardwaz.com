94dce462157fa8b247931c6e5dc615d3a276b19c
himanshubhardwaz <himanshu76200@gmail.com>
2025-03-14 12:07:36 +0530

    feat: add tags

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import type { CollectionEntry } from "astro:content";
+
 const BASE_URL = import.meta.env.BASE_URL.endsWith("/")
   ? import.meta.env.BASE_URL.substring(0, import.meta.env.BASE_URL.length - 1)
   : import.meta.env.BASE_URL;
@@ -23,4 +25,29 @@ export const trimSlash = (path: string) => {
 
 export const pathEqual = (a: string, b: string) => {
   return trimSlash(a) === trimSlash(b);
+};
+
+export function encodeTag(tag: string) {
+  return tag.replace(/\//g, "%2F").replace(/\s+/g, "-");
+}
+
+export function decodeTag(encodedTag: string) {
+  return decodeURIComponent(encodedTag);
+}
+
+export const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
+  const tags: string[] = posts
+    .filter((post) => !post.data.hide)
+    .flatMap((post) => post.data.tags)
+    .map((tag) => encodeTag(tag));
+  return [...new Set(tags)];
+};
+
+export const getPostsByTag = (
+  posts: CollectionEntry<"blog">[],
+  tag: string
+) => {
+  return posts.filter(
+    (post) => !post.data.hide && post.data.tags.includes(decodeTag(tag))
+  );
 };

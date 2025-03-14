927363e25ba1ccc51252962e7fdf09d1547f9ee1
himanshubhardwaz <himanshu76200@gmail.com>
2025-03-14 13:07:35 +0530

    feat: add rss, sitemap

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
+import { SITE_TITLE } from "../consts";
+
+export async function GET(context: APIContext) {
+  const blogs = await getCollection("blog");
+
+  return rss({
+    title: SITE_TITLE,
+    description:
+      "A personal blog about software engineering, web development, and my journey building with modern technologies.",
+    site: context.site ?? "blog.himanshubhardwaz.com",
+    items: blogs.map((post) => ({
+      ...post.data,
+      pubDate: post.data.date,
+      link: `/blog/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}

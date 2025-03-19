e2ffed3605378bed80c0bfdde9b497d998f505a7
himanshubhardwaz <himanshu76200@gmail.com>
2025-03-19 23:44:29 +0530

    chore: update default theme (dark)

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -1,5 +1,3 @@
-const primaryColorScheme = ""; // "light" | "dark"
-
 // Get theme data from local storage
 const currentTheme = localStorage.getItem("theme");
 
@@ -7,13 +5,10 @@ function getPreferTheme() {
   // return theme value in local storage if it is set
   if (currentTheme) return currentTheme;
 
-  // return primary color scheme if it is set
-  if (primaryColorScheme) return primaryColorScheme;
-
   // return user device's prefer color scheme
-  return window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
+  return window.matchMedia("(prefers-color-scheme: light)").matches
+    ? "light"
+    : "dark";
 }
 
 let themeValue = getPreferTheme();

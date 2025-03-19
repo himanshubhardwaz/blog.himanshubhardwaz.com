fbbc26fcaeb6bb60f92754acc8d9ad5b2ef7f66d
himanshubhardwaz <himanshu76200@gmail.com>
2025-03-19 23:30:52 +0530

    fix: astro content image import, content ts support

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,21 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: () =>
+  schema: ({ image }) =>
     z.object({
+      hide: z.boolean().default(false),
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
       category: z.string(),
       tags: z.array(z.string()),
+      cover: z
+        .object({
+          src: image(),
+          alt: z.string(),
+        })
+        .optional(),
     }),
 });
 
-export const collection = { blog };
+export const collections = { blog };

f7461b24178044031db6b3974d75e667d521311a
himanshubhardwaz <himanshu76200@gmail.com>
2025-03-28 10:18:37 +0530

    fix: shiki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,13 @@ export default defineConfig({
   site: "https://blog.himanshubhardwaz.com",
   integrations: [mdx(), sitemap()],
   output: "static",
+  markdown: {
+    shikiConfig: {
+      wrap: true,
+      themes: {
+        light: "dracula",
+        dark: "github-dark",
+      },
+    },
+  },
 });
